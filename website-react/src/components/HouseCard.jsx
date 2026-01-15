f537d99d7146315a978d370a43b425613b15a012
Kasson Davis <135732515+KassonD@users.noreply.github.com>
2026-01-14 19:29:41 -0600

    Added a comment in House Card

--- website-react/src/components/HouseCard.jsx
+++ website-react/src/components/HouseCard.jsx
@@ -3,7 +3,20 @@ import { useEffect, useState } from "react";
 function HouseCard() {
     return (
         <div className="houseCard">
-            
+            {
+                /*
+                    Implement house card design here with fake test information.
+                    (outside of these brackets)
+
+                    On the website, when you click a customer in the customer table,
+                    these cards will appear.
+
+                    To change the style (css) open "index.css" and go to ".houseCard".
+                    
+                    If you want to change the layout of the cards (parent div) go
+                    to index.css and change ".customer-table div.houses".
+                */
+            }
         </div>
     )
 }
