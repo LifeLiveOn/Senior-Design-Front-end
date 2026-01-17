7afe34e6ab9b0141a041bbe78e3ef8a175c0fd01
Kasson Davis <135732515+KassonD@users.noreply.github.com>
2026-01-17 17:03:10 -0600

    Implemented name search (house cards now work when connected to backend)

--- website-react/src/components/CustomerTable.jsx
+++ website-react/src/components/CustomerTable.jsx
@@ -3,24 +3,20 @@ import HouseCard from "../components/HouseCard";
 
 function CustomerTable() {
     const [customers, setCustomers] = useState([]);
+    const [filteredCustomers, setFilteredCustomers] = useState([]);
+    const [houses, setHouses] = useState([]);
+    const [customersLoaded, setCustomersLoaded] = useState(false);
+    const [housesLoaded, setHousesLoaded] = useState(false);
     const [failed, setFailed] = useState(false);
     const [customerId, setCustomerId] = useState(-1);
-
-    // Shows Houses
-    const toggleCustomerIndex = (id) => {
-        if (customerId === id) {
-            setCustomerId(-1);
-        }
-        else {
-            setCustomerId(id);
-        }
-    }
-
+    const [searchContent, setSearchContent] = useState("");
+    const [searchType, setSearchType] = useState("Name");
+    
     // Load Customers
     useEffect(() => {
         const loadCustomers = async () => {
             try {
-                const res = await fetch(`https://backend-42686524573.europe-west1.run.app/api/v1/customers/`, {
+                const res = await fetch(`http://localhost:8000/api/v1/customers/`, { //https://backend-42686524573.europe-west1.run.app/api/v1/customers/
                     credentials: "include"
                 });
 
@@ -31,6 +27,8 @@ function CustomerTable() {
                 
                 console.log(data);
                 setCustomers(data);
+                setFilteredCustomers(data);
+                setCustomersLoaded(true);
             }
             catch (err) {
                 console.log("Error: ", err);
@@ -41,75 +39,78 @@ function CustomerTable() {
         loadCustomers();
     }, []);
 
+    // Load Houses
+    useEffect(() => {
+        const loadHouses = async () => {
+            try {
+                const res = await fetch(`http://localhost:8000/api/v1/houses/`, { //https://backend-42686524573.europe-west1.run.app/api/v1/customers/
+                    credentials: "include"
+                });
+
+                if (!res.ok || res == null)
+                    throw new Error(res.status);
+
+                const data = await res.json();
+                
+                console.log(data);
+                setHouses(data);
+                setHousesLoaded(true);
+            }
+            catch (err) {
+                console.log("Error: ", err);
+                setFailed(true);
+            }
+        }
+
+        if (customerId >= 0) {
+            loadHouses();
+        }
+    }, [customerId]);
+
+    // Toggle Customer ID
+    const toggleCustomerId = async (id) => {
+        setHousesLoaded(false);
+
+        if (customerId === id) {
+            setCustomerId(-1);
+        }
+        else {
+            setCustomerId(id);
+        }
+    }
+
+    // Search
+    const search = () => {
+        setFilteredCustomers(
+            customers.filter(customer => {
+                return customer.name.toLowerCase().includes(searchContent.toLowerCase())
+            })
+        );
+    }
+
+    const setSearchInput = (event) => {
+        setSearchContent(event.target.value)
+    }
+
+    const setSelectInput = (event) => {
+        setSearchType(event.target.value)
+    }
+
     // Display table
     if (failed) {
         return <h1>Database connection failed</h1>;
-        // return (
-        //     <>
-        //         <table className="customer-table">
-        //             <thead>
-        //                 <tr>
-        //                     <th>Date</th>
-        //                     <th>Name</th>
-        //                     <th>Number</th>
-        //                 </tr>
-        //             </thead>
-        //             <tbody>
-        //                 <tr  className="clickable" onClick={() => toggleCustomerIndex(0)}>
-        //                     <td>43242</td>
-        //                     <td>fdsafdsa</td>
-        //                     <td>321321321</td>
-        //                 </tr>
-        //                 {
-        //                     customerId === 0 && (
-        //                         <tr className="expanded">
-        //                             <td colSpan={3}>
-        //                                 <div className="houses">
-        //                                     <HouseCard id={0}></HouseCard>
-        //                                     <HouseCard id={1}></HouseCard>
-        //                                     <HouseCard id={2}></HouseCard>
-        //                                     <HouseCard id={3}></HouseCard>
-        //                                     <HouseCard id={4}></HouseCard>
-        //                                 </div>
-        //                             </td>
-        //                         </tr>
-        //                     )
-        //                 }
-        //                 <tr  className="clickable" onClick={() => toggleCustomerIndex(1)}>
-        //                     <td>43242</td>
-        //                     <td>fdsafdsa</td>
-        //                     <td>321321321</td>
-        //                 </tr>
-        //                 {
-        //                     customerId === 1 && (
-        //                         <tr className="expanded">
-        //                             <td colSpan={3}>
-        //                                 <div className="houses">
-        //                                     <HouseCard id={5}></HouseCard>
-        //                                     <HouseCard id={6}></HouseCard>
-        //                                     <HouseCard id={7}></HouseCard>
-        //                                 </div>
-        //                             </td>
-        //                         </tr>
-        //                     )
-        //                 }
-        //             </tbody>
-        //         </table>
-        //     </>
-
-        // );
     }
-    else {
+    else if (customersLoaded) {
         return (
             <>
                 <h1>Customer Search</h1>
                 <div className="customer-search">
-                    <input type="search" placeholder="Search"></input>
-                    <select name="Sort">
+                    <input type="search" placeholder="Search" onChange={setSearchInput}></input>
+                    <select name="Sort" onChange={setSelectInput}>
                         <option value="Name">Name</option>
                         <option value="Number">Number</option>
                     </select>
-                    <button>Search</button>
+                    <button onClick={search}>Search</button>
                 </div>
                 <table className="customer-table">
                     <thead>
@@ -120,12 +121,31 @@ function CustomerTable() {
                         </tr>
                     </thead>
                     <tbody> {
-                        customers.map((customer) => (
-                            <tr key={customer.id}>
-                                <td>{customer.created_at.substring(0, 10)}</td>
-                                <td>{customer.name}</td>
-                                <td>{customer.phone}</td>
-                            </tr>
+                        filteredCustomers.map((customer) => (
+                            <>
+                                <tr key={customer.id} className="clickable" onClick={() => toggleCustomerId(customer.id)}>
+                                    <td>{customer.created_at.substring(0, 10)}</td>
+                                    <td>{customer.name}</td>
+                                    <td>{customer.phone}</td>
+                                </tr>
+                                {customerId === customer.id && (
+                                        <tr className="expanded">
+                                            <td colSpan={3}>
+                                                <div className="houses">
+                                                    {housesLoaded && houses.map((house) => (
+                                                            <HouseCard id={house.id}></HouseCard>
+                                                        ))
+                                                    }
+                                                    {!housesLoaded &&
+                                                        <p>Loading...</p>
+                                                    }
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    )
+                                }
+                            </>
+                            
                         ))
                     }
                     </tbody>
@@ -133,6 +153,9 @@ function CustomerTable() {
             </>
 
         );
+    }
+    else {
+        return <h1>Loading...</h1>;
     }
 }
 
