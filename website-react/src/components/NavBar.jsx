50487bc9da2cba97e96459a73a6ea97f4c764b1e
jollyserah <serahjolly123@gmail.com>
2026-03-14 16:54:23 -0500

    nav bar changes

--- website-react/src/components/NavBar.jsx
+++ website-react/src/components/NavBar.jsx
@@ -3,7 +3,13 @@ import {Link} from "react-router-dom"
 function NavBar() {
     return (
         <nav className="nav-bar">
-            <Link to="/" className="site-title">Roof-Vision</Link>
+            <Link to="/" className="site-title">
+                <div>
+                    < span className="title-over">Over</span>
+                    <span className="title-watch">Watch</span>
+                </div>
+                <span className="title-sub">by Steak Farm</span>
+            </Link>
             <ul>
                 <li>
                     <Link to="/customers">Customers</Link>
@@ -12,6 +18,7 @@ function NavBar() {
                     <Link to="/about">About</Link>
                 </li>
             </ul>
+            <button className="nav-logout">Log Out</button>
         </nav>
     );
 }
